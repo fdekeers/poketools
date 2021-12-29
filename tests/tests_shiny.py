91fa4b936456307e3b966df47c7b3f5b3ce3996c
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2021-12-29 17:33:12 +0100

    Removed unused files

--- tests/tests_shiny.py
+++ tests/tests_shiny.py
@@ -4,7 +4,7 @@ import audio_template_finder as atf
 from pathlib import Path
 
 
-TEMPLATE_FILE = "template_sounds/shiny/template_cropped.wav"
+TEMPLATE_FILE = "../template_sounds/shiny/template_cropped.wav"
 CORRELATION_DIR = "correlations"
 
 
@@ -19,35 +19,35 @@ def run_test(file_to_test, correlation_file):
 
 class TestBase(unittest.TestCase):
     def test_base(self):
-        file_to_test = "template_sounds/shiny/test/base.wav"
+        file_to_test = "../template_sounds/shiny/test/base.wav"
         correlation_file = f"{CORRELATION_DIR}/correlation_base.png"
         self.assertFalse(run_test(file_to_test, correlation_file))
 
 
 class TestPokeball(unittest.TestCase):
     def test_pokeball(self):
-        file_to_test = "template_sounds/shiny/test/pokeball.wav"
+        file_to_test = "../template_sounds/shiny/test/pokeball.wav"
         correlation_file = f"{CORRELATION_DIR}/correlation_pokeball.png"
         self.assertFalse(run_test(file_to_test, correlation_file))
 
 
 class TestIntimidate(unittest.TestCase):
     def test_intimidate(self):
-        file_to_test = "template_sounds/shiny/test/intimidate.wav"
+        file_to_test = "../template_sounds/shiny/test/intimidate.wav"
         correlation_file = f"{CORRELATION_DIR}/correlation_intimidate.png"
         self.assertFalse(run_test(file_to_test, correlation_file))
 
 
 class TestShiny(unittest.TestCase):
     def test_shiny(self):
-        file_to_test = "template_sounds/shiny/test/shiny.wav"
+        file_to_test = "../template_sounds/shiny/test/shiny.wav"
         correlation_file = f"{CORRELATION_DIR}/correlation_shiny.png"
         self.assertTrue(run_test(file_to_test, correlation_file))
 
 
 class TestExternal(unittest.TestCase):
     def test_shiny(self):
-        file_to_test = "template_sounds/shiny/test/rayquaza.wav"
+        file_to_test = "../template_sounds/shiny/test/rayquaza.wav"
         correlation_file = f"{CORRELATION_DIR}/correlation_external.png"
         self.assertTrue(run_test(file_to_test, correlation_file))
 
